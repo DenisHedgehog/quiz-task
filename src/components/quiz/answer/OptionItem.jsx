b922a820d1d274faa51a6893aa6fbe41eed938dc
DenisEzhov <d.ezhov@rbkmoney.com>
2018-08-27 14:44:25 +0300

    css was replaced by styled components

--- src/components/quiz/answer/OptionItem.jsx
+++ src/components/quiz/answer/OptionItem.jsx
@@ -1,13 +1,52 @@
 import React from 'react';
+import styled from 'styled-components';
+import optionStyle from '../../../constants/optionStyle.js';
 
-const OptionItem = ({style, option, currentOptionId, onCurrentOptionChange}) => (
-    <div className={style} onClick={() => onCurrentOptionChange(option.id)} >
+const StyledOption = styled.div`
+    background-color: ${(props) => {
+        switch (props.optionStyle) {
+            case optionStyle.CORRECT:
+                return 'rgba(101, 192, 101, 0.295)';
+            case optionStyle.INCORRECT:
+                return 'rgba(207, 67, 67, 0.295)';
+            default:
+                return 'white';
+        }
+    }
+    };
+    border: 2px solid;
+    border-color: ${(props) => {
+        switch (props.optionStyle) {
+            case optionStyle.CORRECT:
+                return 'rgba(35, 194, 35, 0.295)';
+            case optionStyle.INCORRECT:
+                return 'rgba(146, 44, 44, 0.295)';
+            default:
+                return 'lightgray';
+        }
+    }
+    };
+    border-radius: 4px;
+    padding: 0 16px;
+    width: 500px;
+    display: flex;
+    align-items: center;
+    margin: 8px;
+    cursor: pointer;
+
+    @media (max-width: 550px) {
+        width: 90vw;
+    }
+`;
+
+const OptionItem = ({ style, option, currentOptionId, onCurrentOptionChange }) => (
+    <StyledOption optionStyle={style} onClick={() => onCurrentOptionChange(option.id)} >
         <input
             name="answerItem"
             type="radio"
             checked={option.id === currentOptionId} />
         <p>{option.option}</p>
-    </div>
+    </StyledOption>
 )
 
 export default OptionItem;
--- src/components/quiz/control/AnswerStatus.jsx
+++ src/components/quiz/control/AnswerStatus.jsx
@@ -1,14 +1,26 @@
 import React from 'react';
 import ANSWER_STAGE from '../../../constants/answerStage.js';
+import styled from 'styled-components';
 
-const AnswerStatus = ({stage, isOptionRight}) => {
-        let status = '';
-        if (stage === ANSWER_STAGE.NEXT) {
-            status = isOptionRight() ? 
-            <div className="status status_correct control__status text_size_big">✓</div> : 
-            <div className="status status_incorrect control__status text_size_big">⨉</div>;
-        }
-        return status
-}
+const StyledStatus = styled.div`
+    height: 50px;
+    width: 50px;
+    font-size: 16px;
+    border-radius: 50%;
+    position: absolute;
+    right: 60%;
+    display:flex;
+    align-items: center;
+    justify-content:center;
+    color: white;
+    background-color: ${(props) => props.isOptionRight() ? 'green' : 'brown'};
+    visibility: ${(props) => props.stage === ANSWER_STAGE.NEXT ? 'visible' : 'hidden'};
+    @media (max-width: 700px) {
+        right: 80%;
+    }
+`;
+
+const AnswerStatus = ({stage, isOptionRight}) => 
+    <StyledStatus stage={stage} isOptionRight={isOptionRight}>{isOptionRight() ? '✓' : '⨉'}</StyledStatus>
 
 export default AnswerStatus;
